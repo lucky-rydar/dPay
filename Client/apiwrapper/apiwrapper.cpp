a89fe7cba21ba69dad624aed7afcb2b6f8c95d7d
Mikhail Didur <mihasyaterminator@gmail.com>
2021-05-03 10:49:12 +0300

    Added all card methods to wrapper

--- Client/apiwrapper/apiwrapper.cpp
+++ Client/apiwrapper/apiwrapper.cpp
@@ -20,6 +20,36 @@ QString ApiWrapper::change_phone(QString token, QString new_phone)
     return makeRequest(baseUrl + "user/change_phone/" + token + "/" + new_phone);
 }
 
+QString ApiWrapper::add(QString token, QString number, QString month_exp, QString year_exp, QString cvv)
+{
+    return makeRequest(baseUrl + "card/add/" + token + "/" + number + "/" + month_exp + "/" + year_exp + "/" + cvv);
+}
+
+QString ApiWrapper::remove(QString token, QString card_id)
+{
+    return makeRequest(baseUrl + "card/remove/" + token + "/" + card_id);
+}
+
+QString ApiWrapper::cards(QString token)
+{
+    return makeRequest(baseUrl + "card/cards/" + token);
+}
+
+QString ApiWrapper::rename(QString token, QString card_id, QString new_name)
+{
+    return makeRequest(baseUrl + "card/rename/" + token + "/" + card_id + "/" + new_name);
+}
+
+QString ApiWrapper::set_default(QString token, QString card_id)
+{
+    return makeRequest(baseUrl + "card/set_default/" + token + "/" + card_id);
+}
+
+QString ApiWrapper::get_card_data(QString token, QString card_id)
+{
+    return makeRequest(baseUrl + "card/get_card_data/" + token + "/" + card_id);
+}
+
 QString ApiWrapper::makeRequest(QString url)
 {
     QNetworkAccessManager accessManager;
--- Client/apiwrapper/apiwrapper.h
+++ Client/apiwrapper/apiwrapper.h
@@ -18,6 +18,12 @@ public:
     Q_INVOKABLE QString change_phone(QString token, QString new_phone);
 
     // card
+    Q_INVOKABLE QString add(QString token, QString number, QString month_exp, QString year_exp, QString cvv);
+    Q_INVOKABLE QString remove(QString token, QString card_id);
+    Q_INVOKABLE QString cards(QString token);
+    Q_INVOKABLE QString rename(QString token, QString card_id, QString new_name);
+    Q_INVOKABLE QString set_default(QString token, QString card_id);
+    Q_INVOKABLE QString get_card_data(QString token, QString card_id);
 
     // transaction
 
